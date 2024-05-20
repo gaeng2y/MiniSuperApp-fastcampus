c336736a26799ee0f87cb0de9ef09904e1318243
gaeng2y <gaeng2y@gmail.com>
2024-05-20 22:28:15 +0900

    1부 03. 복잡한 뷰 만들기 50%

--- MiniSuperApp/AppHome/AppHomeBuilder.swift
+++ MiniSuperApp/AppHome/AppHomeBuilder.swift
@@ -4,32 +4,34 @@ public protocol AppHomeDependency: Dependency {
 }
 
 final class AppHomeComponent: Component<AppHomeDependency>, TransportHomeDependency {
+    // 리블렛에 로직이 추가될 때 로직에 필요한 객체를 담는 바구니
 }
 
 // MARK: - Builder
 
 public protocol AppHomeBuildable: Buildable {
-  func build(withListener listener: AppHomeListener) -> ViewableRouting
+    func build(withListener listener: AppHomeListener) -> ViewableRouting
 }
 
 public final class AppHomeBuilder: Builder<AppHomeDependency>, AppHomeBuildable {
-  
-  public override init(dependency: AppHomeDependency) {
-    super.init(dependency: dependency)
-  }
-  
-  public func build(withListener listener: AppHomeListener) -> ViewableRouting {
-    let component = AppHomeComponent(dependency: dependency)
-    let viewController = AppHomeViewController()
-    let interactor = AppHomeInteractor(presenter: viewController)
-    interactor.listener = listener
     
-    let transportHomeBuilder = TransportHomeBuilder(dependency: component)
+    public override init(dependency: AppHomeDependency) {
+        super.init(dependency: dependency)
+    }
     
-    return AppHomeRouter(
-      interactor: interactor,
-      viewController: viewController,
-      transportHomeBuildable: transportHomeBuilder
-    )
-  }
+    // 리스너를 통해 델리게이트 패턴 구현
+    public func build(withListener listener: AppHomeListener) -> ViewableRouting {
+        let component = AppHomeComponent(dependency: dependency)
+        let viewController = AppHomeViewController()
+        let interactor = AppHomeInteractor(presenter: viewController)
+        interactor.listener = listener
+        
+        let transportHomeBuilder = TransportHomeBuilder(dependency: component)
+        
+        return AppHomeRouter(
+            interactor: interactor,
+            viewController: viewController,
+            transportHomeBuildable: transportHomeBuilder
+        )
+    }
 }
--- MiniSuperApp/FinanceHome/FinanceHomeBuilder.swift
+++ MiniSuperApp/FinanceHome/FinanceHomeBuilder.swift
@@ -1,32 +1,43 @@
 import ModernRIBs
 
 protocol FinanceHomeDependency: Dependency {
-  // TODO: Declare the set of dependencies required by this RIB, but cannot be
-  // created by this RIB.
+    // TODO: Declare the set of dependencies required by this RIB, but cannot be
+    // created by this RIB.
 }
 
-final class FinanceHomeComponent: Component<FinanceHomeDependency> {
-  
-  // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+final class FinanceHomeComponent: Component<FinanceHomeDependency>, SuperPayDashboardDependency {
+    var balance: ReadOnlyCurrentValuePublisher<Double> { balancePublisher }
+    private let balancePublisher: CurrentValuePublisher<Double>
+    
+    init(dependency: FinanceHomeDependency,
+         balancePublisher: CurrentValuePublisher<Double>) {
+        self.balancePublisher = balancePublisher
+        super.init(dependency: dependency)
+    }
 }
 
 // MARK: - Builder
 
 protocol FinanceHomeBuildable: Buildable {
-  func build(withListener listener: FinanceHomeListener) -> FinanceHomeRouting
+    func build(withListener listener: FinanceHomeListener) -> FinanceHomeRouting
 }
 
 final class FinanceHomeBuilder: Builder<FinanceHomeDependency>, FinanceHomeBuildable {
-  
-  override init(dependency: FinanceHomeDependency) {
-    super.init(dependency: dependency)
-  }
-  
-  func build(withListener listener: FinanceHomeListener) -> FinanceHomeRouting {
-    let _ = FinanceHomeComponent(dependency: dependency)
-    let viewController = FinanceHomeViewController()
-    let interactor = FinanceHomeInteractor(presenter: viewController)
-    interactor.listener = listener
-    return FinanceHomeRouter(interactor: interactor, viewController: viewController)
-  }
+    
+    override init(dependency: FinanceHomeDependency) {
+        super.init(dependency: dependency)
+    }
+    
+    func build(withListener listener: FinanceHomeListener) -> FinanceHomeRouting {
+        let balancePublisher = CurrentValuePublisher<Double>(0)
+        let component = FinanceHomeComponent(dependency: dependency, balancePublisher: balancePublisher)
+        let viewController = FinanceHomeViewController()
+        let interactor = FinanceHomeInteractor(presenter: viewController)
+        interactor.listener = listener
+        
+        
+        let superPayDashboardBuilder = SuperPayDashboardBuilder(dependency: component)
+        
+        return FinanceHomeRouter(interactor: interactor, viewController: viewController, superPayDashboardBuildable: superPayDashboardBuilder)
+    }
 }
